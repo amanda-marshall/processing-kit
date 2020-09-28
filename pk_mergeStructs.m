b258e19ae3efbbdf9faa816830b6639cec7aad74
Amanda Claire Marshall <Amanda@Amandas-MacBook-Pro.local>
2020-09-28 09:27:46 +0200

    chore: suppress output

--- pk_mergeStructs.m
+++ pk_mergeStructs.m
@@ -6,7 +6,7 @@ function structB = pk_mergeStructs(structA, structB)
 
   f = fieldnames(structA);
   for i = 1:length(f)
-     structB.(f{i}) = structA.(f{i})
+     structB.(f{i}) = structA.(f{i});
   end
 
-end;
+end
