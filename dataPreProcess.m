66e5db2abe2449955c7f95a1187f4676f38f8b54
Amanda Claire Marshall <Amanda@Amandas-MacBook-Pro.local>
2020-09-28 09:18:13 +0200

    feat: add pre-processing function

--- dataPreProcess.m
+++ dataPreProcess.m
@@ -1,124 +1,92 @@
-clear all
-plist = [12];
-addpath('F:\oneDrive\lmuprojects\fieldtrip-20181029')
-ft_defaults()
-for i=1:length(plist)
-    clear eegdata components exclIndex
-    p = plist(i);
-    pnum = num2str(p);
-    if p < 10
-        pnum = ['0' pnum];
-    end
-    
-    disp(['participant ' pnum ': importing to fieldtrip--------------------']);
-    cfg = [];
-    cfg.datafile     = ['eeg1_raweeg/motorLearn00' pnum '.eeg'];
-    dat_raw = ft_preprocessing(cfg);
-%     
-%     %this label was spelled incorrectly in first data sets
-%     dat_raw.label{1} = 'FP1';
+function dataPreProcess(filePath)
 
+outputPath = fileparts(filePath);
 
-  
-    if exist('exclIndex','var')
-        numchannels =length(dat_raw.label);
-        cfg         = [];
-        cfg.channel = setdiff(1:numchannels, exclIndex);
-        dat_raw        = ft_selectdata(cfg, dat_raw);
-    end
+% Settings
+prefs = [];
+prefs.eegFile = filePath; % File to read
+prefs.ecgChannel = 'ECG';
+prefs.eegChannels = { 'Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6', 'T7', 'C3', 'Cz', 'C4', 'T8', 'TP9', 'CP5', 'CP1', 'CP2', 'CP6', 'TP10', 'P7', 'P3', 'Pz', 'P4', 'P8', 'PO9', 'O1', 'Oz', 'O2', 'PO10', 'AF7', 'AF3', 'AF4', 'AF8', 'F5', 'F1', 'F2', 'F6', 'F9', 'FT7', 'FC3', 'FC4', 'FT8', 'F10', 'C5', 'C1', 'C2', 'C6', 'TP7', 'CP3', 'CPz', 'CP4', 'TP8', 'P5', 'P1', 'P2', 'P6', 'PO7', 'POz', 'PO3', 'P03', 'PO4', 'PO8' };
+% NOTE: This channel configuration contains both the correct PO3 and
+% incorrect P03 channel (P ZERO 3), for misconfigured datasets.
 
-%adding the implicit reference channel to the data
-   
-    %last channel = ECG
-    numchannels =length(dat_raw.label);
-    cfg         = [];
-    cfg.channel = 1:numchannels-1;
-    dataNoECG        = ft_selectdata(cfg, dat_raw);
-    %switching to average reference of all channels
-    cfg = [];
-    cfg.implicitref   = 'FCz';
-    cfg.reref         = 'yes';
-    cfg.refchannel    = 'all';
-    cfg.refmethod     = 'avg';
-    dataNoECG = ft_preprocessing(cfg,dataNoECG);
-    
-    
-    
+% Read raw EEG data into MATLAB
+cfg = [];
+cfg.dataset = prefs.eegFile;
+cfg.channel = prefs.eegChannels;
 
-    
-    
-    
-    cfg         = [];
-    cfg.channel = {'ECG'};
-    ecgOnly      = ft_selectdata(cfg, dat_raw);
-    dat_raw =      ft_appenddata(cfg, dataNoECG,ecgOnly);
-    
-    cfg = [];
-    cfg.lpfilter = 'yes';
-    cfg.lpfreq = 40;
-    cfg.hpfilter = 'yes';
-    cfg.hpfreq = 1;
-    dat_raw = ft_preprocessing(cfg,dat_raw);
-    
-    
-    
-    
-    cfg = [];
-    cfg.dataset             = ['eeg1_raweeg/motorLearn00' pnum '.vhdr'];
-    cfg.trialdef.eventtype = 'Stimulus';
-    cfg.trialdef.eventvalue = {'S111','S112','S113','S114','S121','S122','S123','S124'};
-    cfg.trialdef.prestim    = 1.5;
-    cfg.trialdef.poststim    = 4.5;
-    trialinfo        = ft_definetrial(cfg);
-    trl = trialinfo.trl;
-    
-    eegdata = ft_redefinetrial(trialinfo, dat_raw);
-    
-    
-    %downsampling and switching to single precision since this is more
-    %economically
-    cfg = [];
-    cfg.resamplefs = 250;
-    cfg.detrend    = 'no';
-    eegdata = ft_resampledata(cfg, eegdata);
-    %     smplpos = 1;
-    numtrials = size(eegdata.trial,2);
-    for tnum=1:numtrials
-        %         trl(tnum,1) = smplpos;
-        trl(tnum,2) = trl(tnum,1) + size(eegdata.trial{tnum},2)-1;
-        %         smplpos = trl(tnum,2)+1;
-        trl(tnum,3) = -1*(find(eegdata.time{1}>=0,1)-1); %all samples before point 0
-    end
-    % eegdata.cfg.trl = trl;
-    cfg = [];
-    cfg.precision = 'single';
-    eegdata = ft_preprocessing(cfg,eegdata);
-    
-    %
-    clear dataNoECG ecgOnly dat_raw
-    
-    %no splitting up data again, because ECG is not part of the ICA
-        %last channel = ECG
-    numchannels =length(eegdata.label);
-    cfg         = [];
-    cfg.channel = 1:numchannels-1;
-    dataNoECG        = ft_selectdata(cfg, eegdata);
-        
-    cfg         = [];
-    cfg.channel = {'ECG'};
-    ecgOnly      = ft_selectdata(cfg, eegdata);
-    
-    
-    cfg        = [];
-    cfg.method = 'runica'; % this is the default and uses the implementation from EEGLAB
-    cfg.runica.extended = 0;
-    components = ft_componentanalysis(cfg, dataNoECG);
-    
-    eegdata = dataNoECG;
-    eegdata.cfg.trl = trl;
-    save(['eeg2_ICA/learnEEG' num2str(p)], 'eegdata');
-   
-    save(['eeg2_ICA/learnCOMP' num2str(p)], 'components');
-     save(['eeg2_ICA/learnECG' num2str(p)], 'ecgOnly');
-    
-end
+% Read in EEG Data
+fprintf('\n### 1. Reading EEG Data\n\n');
+data_EEG = ft_preprocessing(cfg);
+
+% Read in ECG Data
+fprintf('\n### 2. Reading ECG Data\n\n');
+cfg.channel = prefs.ecgChannel;
+data_ECG = ft_preprocessing(cfg);
+
+% Set up EEG filters
+cfg = [];
+cfg.hpfilter = 'yes';
+cfg.lpfilter = 'yes';
+cfg.hpfreq = 1;
+cfg.lpfreq = 40;
+
+% Set up EEG re-referencing
+cfg.reref = 'yes';
+cfg.refmethod = 'avg';
+cfg.refchannel = 'all';
+
+% Filter and re-reference EEG Data
+fprintf('\n### 3. Filtering + Re-referencing EEG Data\n\n');
+data_EEG = ft_preprocessing(cfg, data_EEG);
+
+% NOTE: We used to segment the data at this point, but we will instead do this
+% after filtering, re-referencing, component analysing and CFA detection.
+
+% Combine EEG and ECG Datasets
+fprintf('\n### 4. Combine EEG + ECG Datasets\n\n');
+cfg = [];
+data = ft_appenddata(cfg, data_EEG, data_ECG);
+
+% Create a trial definition configuration
+fprintf('\n### 5. Define trial windows for segmentation\n\n');
+cfg = [];
+cfg.dataset = prefs.eegFile;
+cfg.trialfun = 'ft_trialfun_bracketed'; % Custom trial definition function that uses triggers as markers for the start and end of the trial
+cfg.trialdef.eventtype = 'Stimulus'; % Trial event type
+cfg.trialdef.eventvalue = {'S  1', 'S  2', 'S  3'}; % Event codes that mark the start of a trial
+cfg.trialdef.eventvalueend = {'S 30', 'S 31', 'S 32', 'S 33', 'S 34', 'S 35', 'S 36', 'S 37', 'S 38', 'S 39', 'S 40', 'S 41'}; % Event codes that mark the end of a trial
+cfg.trialdef.prestim = 0; % Time (in s) before the start trigger
+cfg.trialdef.poststim = 2; % Time (in s) after the end trigger
+trial_definition = ft_definetrial(cfg);
+
+% Segment data based on trial definition
+fprintf('\n### 6. Segment merged dataset into full trial windows\n\n');
+data_segmented = ft_redefinetrial(trial_definition, data);
+
+% Split data into EEG and ECG after segmentation
+fprintf('\n### 7. Split dataset into EEG and ECG after segmentation\n\n');
+cfg = [];
+cfg.channel = prefs.eegChannels;
+data_EEG = ft_selectdata(cfg, data_segmented);
+cfg.channel = prefs.ecgChannel;
+data_ECG = ft_selectdata(cfg, data_segmented);
+
+% Run Independent Component Analysis
+fprintf('\n### 8. Run Independent Component Analysis\n\n');
+cfg = [];
+cfg.method = 'runica'; % This is the default and uses the implementation from EEGLAB
+cfg.runica.extended = 0;
+data_components = ft_componentanalysis(cfg, data_EEG);
+
+% Save processed data
+fprintf('\n### 9. Save data to disk\n\n');
+filePathEEG = fullfile(outputPath, 'processed_data_EEG');
+filePathECG = fullfile(outputPath, 'processed_data_ECG');
+filePathComponents = fullfile(outputPath, 'processed_data_components');
+save(filePathEEG, 'data_EEG');
+save(filePathECG, 'data_ECG');
+save(filePathComponents, 'data_components');
+
+% Done
+fprintf('\n### Done\n\n');
