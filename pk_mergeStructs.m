0f13fba3bbc53b0b9401c797a2e01f4181368487
Amanda Claire Marshall <Amanda@Amandas-MacBook-Pro.local>
2020-09-28 09:26:56 +0200

    docs: add function description

--- pk_mergeStructs.m
+++ pk_mergeStructs.m
@@ -1,4 +1,8 @@
 function structB = pk_mergeStructs(structA, structB)
+% pk_mergeStructs: Merge two structures
+
+% Returns a merged structure. If Structure B contains fields that also exist in
+% structure A, fields from B will override A.
 
   f = fieldnames(structA);
   for i = 1:length(f)
