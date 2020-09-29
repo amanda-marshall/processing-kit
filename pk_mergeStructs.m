cfedd4d9418597fcb59e953694aad8f1d2081479
Amanda Claire Marshall <Amanda@Amandas-MacBook-Pro.local>
2020-09-29 22:20:00 +0200

    feat: allow recursive merging of structs

--- pk_mergeStructs.m
+++ pk_mergeStructs.m
@@ -1,12 +1,19 @@
-function structB = pk_mergeStructs(structA, structB)
+function structA = pk_mergeStructs(structA, structB)
 % pk_mergeStructs: Merge two structures
 
 % Returns a merged structure. If Structure B contains fields that also exist in
 % structure A, fields from B will override A.
 
-  f = fieldnames(structA);
+  f = fieldnames(structB);
   for i = 1:length(f)
-     structB.(f{i}) = structA.(f{i});
+    fieldname = f{i};
+    % If both structs contain the same field and both are structs, deep merge them...
+    if isfield(structA, fieldname) && isstruct(structA.(fieldname)) && isfield(structB, fieldname) && isstruct(structB.(fieldname))
+      structA.(fieldname) = pk_mergeStructs(structA.(fieldname), structB.(fieldname));
+    % ...otherwise, just overrride it
+    else
+      structA.(fieldname) = structB.(fieldname);
+    end
   end
 
 end
